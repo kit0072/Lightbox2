cee664dc653fb8e4f1714716b86ee4557693694d
kit0072 <110324690+kit0072@users.noreply.github.com>
2023-03-23 12:58:25 +0800

    Update 2222.js

--- 2222.js
+++ 2222.js
@@ -1,1 +1,26 @@
 # Lightbox2
+
+    $.getScript("https://sites.google.com/site/asblin3/lightbox2/jquery.lightbox-0.5.pack.js", function(){
+        $("head").append($("<link>").attr({
+            rel: "stylesheet",
+            type: "text/css",
+            href: "https://raw.githubusercontent.com/kit0072/Lightbox2/main/jquery.lightbox-0.5.css",
+            media: "screen"
+        }));
+        $.each($("div.post > div.post-body"), function(i, $obj){
+            $.each($(this).find("a:has(img)"), function(){
+                if ($(this).attr("href").indexOf("amazon.com") == -1) 
+                    $(this).addClass("lightbox-group" + i).attr({
+                        href: $(this).attr("href").replace(/s1600-h/g, "s1600")
+                    });
+            });
+            $(".lightbox-group" + i).lightBox({
+                imageLoading: 'https://raw.githubusercontent.com/kit0072/Lightbox2/main/lightbox-ico-loading.gif',
+                imageBtnClose: 'https://raw.githubusercontent.com/kit0072/Lightbox2/main/lightbox-btn-close.gif',
+                imageBlank: 'https://raw.githubusercontent.com/kit0072/Lightbox2/main/lightbox-blank.gif',
+                fixedNavigation: true,
+                overlayOpacity: 0.4,
+                containerResizeSpeed: 500
+            });
+        });
+    });
